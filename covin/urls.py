194530132a72ea8c0aba543eb63d5c181d4c9427
Surya Prakash Maurya <43824111+spm999@users.noreply.github.com>
2022-04-25 22:17:49 +0530

    Update urls.py
    removed url as in django 4.0 url is removed.

--- covin/urls.py
+++ covin/urls.py
@@ -18,13 +18,13 @@ from django.urls import path,include
 from django.conf import settings 
 from django.conf.urls.static import static
 from django.views.static import serve
-from django.conf.urls import url 
+# from django.conf.urls import re_path as url
 
 urlpatterns = [
     path('',include('covi.urls')),
     path('admin/', admin.site.urls),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
      
 ]
 # urlpatterns=urlpatterns+static(settings.MEDIA_URL,documnet_root=setting.MEDIA_ROOT)
